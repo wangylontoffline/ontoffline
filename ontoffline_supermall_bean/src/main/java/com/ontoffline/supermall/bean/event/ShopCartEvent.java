87fca6badbd28af7c68ff7100484910d625b320b
wangylontoffline <wangyonglong568@gmail.com>
2024-05-31 22:00:35 +0800

    修改备注信息

--- ontoffline_supermall_bean/src/main/java/com/ontoffline/supermall/bean/event/ShopCartEvent.java
+++ ontoffline_supermall_bean/src/main/java/com/ontoffline/supermall/bean/event/ShopCartEvent.java
@@ -17,12 +17,8 @@ import java.util.List;
 import com.ontoffline.supermall.bean.app.dto.ShopCartDto;
 import com.ontoffline.supermall.bean.app.dto.ShopCartItemDto;
 
-/*
- * Copyright (c) 2017-2099 南京奥托兰科技有限公司 All rights reserved.
- *
- * https://www.ontoffline.com
- *
- * 保留所有权力，翻版必究
+/**
+ * @author wangylontoffline
  */
 
 @Data
